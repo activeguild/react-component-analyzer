08748fa8830caad8a03e05db46afa72baf403049
j1ngzoue <workdrive0306@gmail.com>
2022-01-04 16:49:29 +0900

    Chang to output 'stats.html' to the hierarchy where the process was executed

--- src/write.ts
+++ src/write.ts
@@ -4,7 +4,7 @@ import path from 'path'
 import { CustomSchema } from './types'
 
 export const writeHtml = (diagram: CustomSchema): void => {
-  const outputFilePath = path.resolve(__dirname, '../dist/stats.html')
+  const outputFilePath = path.resolve(path.resolve(), '../stats.html')
   writeFileSync(outputFilePath, getHtml(diagram))
   console.log(chalk.green(`Success. ${outputFilePath}`))
   return
