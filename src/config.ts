ac4a131b591df13785c16e00e0b020af40c2d88c
j1ngzoue <workdrive0306@gmail.com>
2022-01-05 01:18:38 +0900

    Refactor

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,6 @@
-import { Config } from './types'
+import type { ImportDeclaration } from '@typescript-eslint/types/dist/ast-spec'
+import path from 'path'
+import { Alias, Config } from './types'
 
 export const resolveFinalConfig = (config: Config): Required<Config> => {
   const finalConfig: Required<Config> = {
@@ -10,4 +12,24 @@ export const resolveFinalConfig = (config: Config): Required<Config> => {
     finalConfig.alias = config.alias || []
   }
   return finalConfig
+}
+
+export const resolveAlias = (
+  alias: Alias[],
+  dir: string,
+  importDec: ImportDeclaration
+) => {
+  for (const { find, replacement } of alias) {
+    if (importDec.source.value.startsWith(find)) {
+      const replaced = importDec.source.value.replace(find, replacement)
+
+      if (path.isAbsolute(replaced)) {
+        return importDec.source.value.replace(find, replacement)
+      } else {
+        return path.resolve(dir, replaced)
+      }
+    }
+  }
+
+  return path.resolve(dir, importDec.source.value)
 }
