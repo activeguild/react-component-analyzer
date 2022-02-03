f92b245d6da67ea67ee18b3e50a9d594fbd36260
j1ngzoue <workdrive0306@gmail.com>
2022-02-03 10:56:05 +0900

    Change inline, style and js

--- src/write.ts
+++ src/write.ts
@@ -1,11 +1,21 @@
-import { writeFileSync } from 'fs'
+import { readFileSync, writeFileSync } from 'fs'
 import path from 'path'
 import pc from 'picocolors'
 import { CustomDiagram } from './types'
 
 export const writeHtml = (diagram: CustomDiagram): void => {
   const outputFilePath = path.resolve(path.resolve(), './stats.html')
-  writeFileSync(outputFilePath, makeHtml(diagram))
+  const viewerJsAsString = readFileSync(
+    path.resolve(__dirname, './viewer.js')
+  ).toString()
+  const viewerCssAsString = readFileSync(
+    path.resolve(__dirname, './viewer.css')
+  ).toString()
+
+  writeFileSync(
+    outputFilePath,
+    makeHtml(diagram, viewerJsAsString, viewerCssAsString)
+  )
   console.log(pc.green(`Success. ${outputFilePath}`))
   return
 }
@@ -14,25 +24,25 @@ const getDiagramJson = (diagram: CustomDiagram): string => {
   return `const diagram =${JSON.stringify(diagram)}`
 }
 
-const makeHtml = (diagram: CustomDiagram) => {
-  const resolveFilePath = (filaPath: string) => {
-    return path.resolve(__dirname, filaPath)
-  }
-
+const makeHtml = (
+  diagram: CustomDiagram,
+  viewerJsAsString: string,
+  viewerCssAsString: string
+) => {
   return `<!DOCTYPE html>
 <html>
   <head>
     <meta charset="UTF-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1"/>
     <title></title>
-    <link href="${resolveFilePath('../dist/viewer.css')}" rel="stylesheet" />
+    <style>${viewerCssAsString}</style>
     <script>
     ${getDiagramJson(diagram)}
     </script>
   </head>
   <body>
     <div id="root" />
-    <script src="${resolveFilePath('../dist/viewer.js')}"></script>
+    <script>${viewerJsAsString}</script>
   </body>
 </html>`
 }
